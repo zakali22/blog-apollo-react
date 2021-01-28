dd435ed1129d33075397e3fdf99191696ab53b01
zakali22 <zak.fined@gmail.com>
2021-01-28 00:11:09 +0000

    Connect to mongodb

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const {ApolloServer} = require("apollo-server")
-const resolvers = require("./resolvers")
-const typeDefs = require("./types")
+const resolvers = require("./resolvers/index.js")
+const typeDefs = require("./types/index.js")
+
+const mongoose = require('mongoose');
+const db = mongoose.connection;
+const PORT = process.env.PORT || 4000;
 
 const server = new ApolloServer({
     typeDefs,
@@ -8,3 +12,16 @@ const server = new ApolloServer({
     playground: true,
     introspection: true
 })
+
+
+mongoose.connect('mongodb+srv://zakali22:wJ2cQEa5ptGBJDjG@cluster0.z9crl.mongodb.net/blog-apollo-db?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true});
+
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log("✅ Database connected ✅" )
+
+    server.listen().then(({url}) => console.log(`Listening on port http://localhost:${PORT}`))
+
+});
+
