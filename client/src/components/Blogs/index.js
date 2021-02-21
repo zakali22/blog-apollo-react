2a9f6e03a9c4c52d7423cd2beaca23573af19259
zakali22 <zak.fined@gmail.com>
2021-02-21 20:06:30 +0000

    Add blogs section

---
+++ client/src/components/Blogs/index.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import {Link} from "react-router-dom"
+import {ReactComponent as StuckHome} from "../../assets/images/stuck-home.svg"
+
+export default class Blogs extends Component {
+    render() {
+        return (
+            <div className="blogs">
+                <div className="container">
+                    <div className="blogs__title">
+                        <h2>Blogs</h2>
+                        <h2 className="blogs__title-bg">Blogs</h2>
+                    </div>
+                    <div className="blogs__listing grid">
+                        <Link to="/" className="blogs__listing-card blogs__listing-card--condensed blog-1">
+                            <div className="blogs__listing-card-image">
+                                <StuckHome />
+                            </div>
+                            <div className="blogs__listing-card-description">
+                                <h3>Blogs article heading</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </Link>
+
+                        <Link to="/" className="blogs__listing-card blogs__listing-card--expanded blog-2">
+                            <div className="blogs__listing-card-image">
+                                <StuckHome />
+                            </div>
+                            <div className="blogs__listing-card-description">
+                                <h3>Blogs article heading</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </Link>
+
+                        <Link to="/" className="blogs__listing-card blogs__listing-card--expanded blog-3">
+                            <div className="blogs__listing-card-image">
+                                <StuckHome />
+                            </div>
+                            <div className="blogs__listing-card-description">
+                                <h3>Blogs article heading</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </Link>
+
+                        <Link to="/" className="blogs__listing-card blogs__listing-card--condensed blog-4">
+                            <div className="blogs__listing-card-image">
+                                <StuckHome />
+                            </div>
+                            <div className="blogs__listing-card-description">
+                                <h3>Blogs article heading</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import Header from "../components/Header"
+import Blogs from "../components/Blogs"
 
 export default class Homepage extends Component {
     render() {
         return (
             <main className="main-content">
                 <Header />
+                <Blogs />
             </main>
         )
     }
