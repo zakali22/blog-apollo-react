335bb753bc25d61029bcc97d00580d8faeec1e77
zakali22 <zak.fined@gmail.com>
2021-02-21 18:33:07 +0000

    Add headroom.js

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Footer extends Component {
     render() {
         return (
-            <div className="footer">
+            <div className="footer" style={{height: "50vh"}}>
                 <div className="container">
                     <p>Footer</p>
                 </div>
--- client/src/components/Layout/Navbar.js
+++ client/src/components/Layout/Navbar.js
@@ -1,10 +1,32 @@
 import React, { Component } from 'react'
 import {Link} from "react-router-dom"
+import Headroom from "headroom.js"
 
 export default class Navbar extends Component {
+    constructor(props){
+        super(props)
+        this.navEl = React.createRef();
+    }
+
+    componentDidMount(){
+        const options = {
+            "offset": 205,
+            "tolerance": 5,
+            "classes": {
+                "initial": "header--fixed",
+                "pinned": "slideDown",
+                "unpinned": "slideUp",
+                "top": "top",
+                "notTop": "not-top"
+            }
+        }
+        const headroom  = new Headroom(this.navEl.current, options);
+        headroom.init();
+    }
+
     render() {
         return (
-            <nav className="navbar">
+            <nav className="navbar" ref={this.navEl}>
                 <div className="container">
                     <Link to="/" className="navbar__logo-wrapper">
                         <div className="navbar__logo-icon">B</div>
